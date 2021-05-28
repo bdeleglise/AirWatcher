b5d4edcc7dc9c2d811cbd657860953e50f0f6156
Mathieu <saugiermathieu@gmail.com>
2021-05-28 16:01:21 +0200

    modif ui compile

--- AirWatcher/View/UserInterface.cpp
+++ AirWatcher/View/UserInterface.cpp
@@ -4,6 +4,46 @@ using namespace std;
 
 #include "../AirWatcher.cpp"
 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "../AirWatcher.cpp"
+
+int GetInput()
+{
+	int choice;
+	cin >> choice;
+	return choice;
+}
+
+void DisplayMainMenu()
+{
+	cout << "Bienvenue dans le logiciel AirWatcher \n"
+		"Pour naviguer dans les menus du programme, entrez le chiffre correspondant à l'action que vous souhaitez réaliser. \n"
+		"\n"
+		"------------Menu principal-------------"
+		"\n"
+		"1 - Afficher des statistiques\n"
+		"2 - Détecter les fraudes\n"
+		<< endl;
+
+}
+
+void DisplayStatsMenu()
+{
+	cout <<
+		"------------Affichage des statistiques-------------\n"
+		"\n"
+		"1 - Afficher la qualité d'air moyenne d'une zone \n"
+		"2 - Afficher les zones avec la meilleure / pire qualité d'air \n"
+		"3 - Sélectionner un capteur \n"
+		"4 - Observer l'impact des AirCleaners \n"
+		<< endl;
+}
+
+
 class BaseMenu
 
 {
@@ -23,8 +63,31 @@ protected:
 	string m_MenuText;
 };
 
+class StatsMenu : public BaseMenu
+{
+public:
+	StatsMenu()
+	{
+		m_MenuText = "------------Affichage des statistiques-------------\n"
+			"\n"
+			"1 - Afficher la qualité d'air moyenne d'une zone \n"
+			"2 - Afficher les zones avec la meilleure / pire qualité d'air \n"
+			"3 - Sélectionner un capteur \n"
+			"4 - Observer l'impact des AirCleaners \n"
+			;
+	}
+
+	BaseMenu* getNextMenu(int choice, bool& iIsQuitOptionSelected)
+		//Définition de la méthode pure virtuelle au dessus.
+	{
+
+	}
+
+};
+
 class MenuUI : public BaseMenu
 {
+public:
 	MenuUI()
 	{
 		m_MenuText = "Bienvenue dans le logiciel AirWatcher \n"
@@ -49,17 +112,4 @@ class MenuUI : public BaseMenu
 	}
 };
 
-class StatsMenu : public BaseMenu
-{
-	StatsMenu()
-	{
-		m_MenuText = "------------Affichage des statistiques-------------\n"
-			"\n"
-			"1 - Afficher la qualité d'air moyenne d'une zone \n"
-			"2 - Afficher les zones avec la meilleure / pire qualité d'air \n"
-			"3 - Sélectionner un capteur \n"
-			"4 - Observer l'impact des AirCleaners \n"
-			;
-	}
 
-}
