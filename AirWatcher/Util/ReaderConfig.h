4f385f5ee5917b3ef360285e4c2c6ccefa1920af
Mathieu <saugiermathieu@gmail.com>
2021-06-02 10:56:11 +0200

    Utilisation du chemin relatif a la solution pour aller dans le dataset

--- AirWatcher/Util/ReaderConfig.h
+++ AirWatcher/Util/ReaderConfig.h
@@ -17,7 +17,9 @@ using namespace std;
 struct Config
 {
     //Redirection par rapport ou l'objet model est créé
-    string DIRECTORYPATH = "../Dataset/";
+    string SOLUTIONDIR = SOLUTION_DIR;
+    string DATASETPATH = R"(Dataset\)";
+    string DIRECTORYPATH = SOLUTIONDIR + DATASETPATH;
     string SENSORFILE = "sensors.csv";
     string MEASUREMENTSFILE = "measurements.csv";
     string ATTRIBUTESFILE = "attributes.csv";
